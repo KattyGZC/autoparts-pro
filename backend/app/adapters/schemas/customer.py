2f3253be58273599ddbc9849fdb08460d0c9ce9f
KattyGZC <kattyko.gzc@gmail.com>
2025-05-18 17:15:02 -0500

    Validation to customer

--- backend/app/adapters/schemas/customer.py
+++ backend/app/adapters/schemas/customer.py
@@ -2,12 +2,14 @@ from typing import Optional
 from uuid import UUID
 from app.adapters.schemas.base import BaseSchema
 from pydantic.networks import EmailStr
+from pydantic import constr
 
 class CustomerBase(BaseSchema):
-    name: str
+    name: constr(min_length=2, max_length=100, strip_whitespace=True)
     email: Optional[EmailStr] = None
-    phone: Optional[str] = None
-    address: Optional[str] = None
+    phone: Optional[constr(min_length=7, max_length=15, regex=r'^\+?[0-9]*$')] = None
+    address: Optional[constr(min_length=5, max_length=150, strip_whitespace=True)] = None
+
 
 class CustomerCreate(CustomerBase):
     pass
